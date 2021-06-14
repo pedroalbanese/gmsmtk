ff1b53a59942d68cd801b9f759a808666ec33d7b
Pedro Albanese <68971450+pedroalbanese@users.noreply.github.com>
2021-06-14 18:49:48 -0300

    Update main.go

--- main.go
+++ main.go
@@ -1,4 +1,5 @@
 package main
+
 import (
 	"bufio"
 	"crypto/cipher"
@@ -7,6 +8,7 @@ import (
 	"encoding/hex"
 	"flag"
 	"fmt"
+	"github.com/pedroalbanese/cmac"
 	"github.com/pedroalbanese/gmcrypto/sm3"
 	"github.com/pedroalbanese/gmcrypto/sm4"
 	"golang.org/x/crypto/pbkdf2"
@@ -17,236 +19,274 @@ import (
 	"strings"
 )
 
-	var check = flag.String("check", "", "Check hashsum file.")
-	var crypt = flag.Bool("crypt", false, "Encrypt/Decrypt with symmetric cipher SM4.")
-	var digest = flag.Bool("digest", false, "Compute single hashsum with SM3.")
-	var iter = flag.Int("iter", 1024, "Iterations. (for PBKDF2)")
-	var key = flag.String("key", "", "Secret key/Password.")
-	var mac = flag.Bool("hmac", false, "Hash-based message authentication code.")
-	var pbkdf = flag.Bool("pbkdf2", false, "PBKDF2.")
-	var random = flag.Bool("rand", false, "Generate random 128-bit cryptographic key.")
-	var recursive = flag.Bool("rec", false, "Process directories recursively.")
-	var salt = flag.String("salt", "", "Salt. (for PBKDF2)")
-	var target = flag.String("hashsum", "", "Target file/wildcard to generate hashsum list.")
-	var verbose = flag.Bool("verb", false, "Verbose mode. (for CHECK command)")
+var (
+	check     = flag.String("check", "", "Check hashsum file.")
+	ciphmac   = flag.Bool("cmac", false, "Cipher-based message authentication code.")
+	crypt     = flag.Bool("crypt", false, "Encrypt/Decrypt with symmetric cipher SM4.")
+	digest    = flag.Bool("digest", false, "Compute single hashsum with SM3.")
+	iter      = flag.Int("iter", 1024, "Iterations. (for PBKDF2)")
+	key       = flag.String("key", "", "Secret key/Password.")
+	mac       = flag.Bool("hmac", false, "Hash-based message authentication code.")
+	pbkdf     = flag.Bool("pbkdf2", false, "Password-based key derivation function.")
+	random    = flag.Bool("rand", false, "Generate random 128-bit cryptographic key.")
+	recursive = flag.Bool("rec", false, "Process directories recursively.")
+	salt      = flag.String("salt", "", "Salt. (for PBKDF2)")
+	target    = flag.String("hashsum", "", "Target file/wildcard to generate hashsum list.")
+	verbose   = flag.Bool("verb", false, "Verbose mode. (for CHECK command)")
+	short     = flag.Bool("short", false, "Generate 64-bit key. (for RAND and PBKDF2 command)")
+)
 
 func main() {
-    flag.Parse()
+	flag.Parse()
 
-        if (len(os.Args) < 2) {
-	fmt.Fprintln(os.Stderr,"GMSM Chinese National Standard Toolkit - ALBANESE Lab (c) 2020-2021\n")
-	fmt.Fprintln(os.Stderr,"Usage of",os.Args[0]+":")
-        flag.PrintDefaults()
-        os.Exit(0)
-        }
+	if len(os.Args) < 2 {
+		fmt.Fprintln(os.Stderr, "GMSM Cipher Suite - Chinese National Standard Toolkit")
+		fmt.Fprintln(os.Stderr, "Copyright (c) 2020-2021 Pedro Albanese. All rights reserved.\n")
+		fmt.Fprintln(os.Stderr, "Usage of", os.Args[0]+":")
+		flag.PrintDefaults()
+		os.Exit(0)
+	}
 
-	if *random == true {
-	var key []byte
-	var err error
+	if *random == true && *short == false {
+		var key []byte
+		var err error
 		key = make([]byte, 16)
 		_, err = io.ReadFull(rand.Reader, key)
 		if err != nil {
-                        log.Fatal(err)
+			log.Fatal(err)
 		}
 		fmt.Println(hex.EncodeToString(key))
-        	os.Exit(0)
+		os.Exit(0)
 	}
 
-        if *pbkdf == true && *crypt == false && *mac == false {
-	prvRaw := pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
-	fmt.Println(hex.EncodeToString(prvRaw))
-	os.Exit(0)
-	}
-
-        if *crypt == true {
-	var keyHex string
-	var prvRaw []byte
-	if *pbkdf == true {
-	prvRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
-	keyHex = hex.EncodeToString(prvRaw)
-	} else {
-	keyHex = *key
-	}
-	var key []byte
-	var err error
-	if keyHex == "" {
-		key = make([]byte, 16)
+	if *random == true && *short == true {
+		var key []byte
+		var err error
+		key = make([]byte, 8)
 		_, err = io.ReadFull(rand.Reader, key)
 		if err != nil {
-                        log.Fatal(err)
+			log.Fatal(err)
 		}
-		fmt.Fprintln(os.Stderr, "Key=", hex.EncodeToString(key))
-	} else {
-		key, err = hex.DecodeString(keyHex)
+		fmt.Println(hex.EncodeToString(key))
+		os.Exit(0)
+	}
+
+	if *crypt == true {
+		var keyHex string
+		var prvRaw []byte
+		if *pbkdf == true {
+			prvRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
+			keyHex = hex.EncodeToString(prvRaw)
+		} else {
+			keyHex = *key
+		}
+		var key []byte
+		var err error
+		if keyHex == "" {
+			key = make([]byte, 16)
+			_, err = io.ReadFull(rand.Reader, key)
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Fprintln(os.Stderr, "Key=", hex.EncodeToString(key))
+		} else {
+			key, err = hex.DecodeString(keyHex)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if len(key) != 16 {
+				log.Fatal(err)
+			}
+		}
+		ciph, err := sm4.NewCipher(key)
 		if err != nil {
-                        log.Fatal(err)
+			log.Fatal(err)
 		}
-		if len(key) != 16 {
-                        log.Fatal(err)
+		iv := make([]byte, 16)
+		stream := cipher.NewCTR(ciph, iv)
+		buf := make([]byte, 128*1<<10)
+		var n int
+		for {
+			n, err = os.Stdin.Read(buf)
+			if err != nil && err != io.EOF {
+				log.Fatal(err)
+			}
+			stream.XORKeyStream(buf[:n], buf[:n])
+			if _, err := os.Stdout.Write(buf[:n]); err != nil {
+				log.Fatal(err)
+			}
+			if err == io.EOF {
+				break
+			}
+		}
+		os.Exit(0)
+	}
+
+	if *digest == true && *target == "" {
+		h := sm3.New()
+		io.Copy(h, os.Stdin)
+		fmt.Println(hex.EncodeToString(h.Sum(nil)))
+		os.Exit(0)
+	}
+
+	if *ciphmac == true {
+		var keyHex string
+		var prvRaw []byte
+		if *pbkdf == true {
+			prvRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
+			keyHex = hex.EncodeToString(prvRaw)
+		} else {
+			keyHex = *key
+			if len(keyHex) != 128/8 {
+				fmt.Println("Secret key must have 64-bit. (try \"-rand 128\")")
+				os.Exit(1)
+			}
+		}
+		c, _ := sm4.NewCipher([]byte(keyHex))
+		h, _ := cmac.New(c)
+		io.Copy(h, os.Stdin)
+		fmt.Println(hex.EncodeToString(h.Sum(nil)))
+		os.Exit(0)
+	}
+
+	if *mac == true {
+		var keyHex string
+		var prvRaw []byte
+		if *pbkdf == true {
+			prvRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
+			keyHex = hex.EncodeToString(prvRaw)
+		} else {
+			keyHex = *key
+		}
+		key, err := hex.DecodeString(keyHex)
+		if err != nil {
+			log.Fatal(err)
+		}
+		h := hmac.New(sm3.New, key)
+		if _, err = io.Copy(h, os.Stdin); err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println(hex.EncodeToString(h.Sum(nil)))
+		os.Exit(0)
+	}
+
+	if *pbkdf == true && *crypt == false && *mac == false && *short == false {
+		prvRaw := pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
+		fmt.Println(hex.EncodeToString(prvRaw))
+		os.Exit(0)
+	}
+
+	if *pbkdf == true && *crypt == false && *mac == false && *short == true {
+		prvRaw := pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 8, sm3.New)
+		fmt.Println(hex.EncodeToString(prvRaw))
+		os.Exit(0)
+	}
+
+	if *target != "" && *recursive == false {
+		files, err := filepath.Glob(*target)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for _, match := range files {
+			h := sm3.New()
+			f, err := os.Open(match)
+			if err != nil {
+				log.Fatal(err)
+			}
+			if _, err := io.Copy(h, f); err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(hex.EncodeToString(h.Sum(nil)), "*"+f.Name())
 		}
 	}
-	ciph, err := sm4.NewCipher(key)
-	if err != nil {
-		log.Fatal(err)
-	}
-	iv := make([]byte, 16)
-	stream := cipher.NewCTR(ciph, iv)
-	buf := make([]byte, 32*1<<10)
-	var n int
-	for {
-		n, err = os.Stdin.Read(buf)
-		if err != nil && err != io.EOF {
-                        log.Fatal(err)
-		}
-		stream.XORKeyStream(buf[:n], buf[:n])
-		if _, err := os.Stdout.Write(buf[:n]); err != nil {
-                        log.Fatal(err)
-		}
-		if err == io.EOF {
-			break
-		}
-	}
-        os.Exit(0)
-	}
-
-        if *digest == true && *target == "" {
-	h := sm3.New()
-	io.Copy(h, os.Stdin)
-	fmt.Println(hex.EncodeToString(h.Sum(nil)))
-        os.Exit(0)
-        }
-
-        if *mac == true {
-	var keyHex string
-	var prvRaw []byte
-	if *pbkdf == true {
-	prvRaw = pbkdf2.Key([]byte(*key), []byte(*salt), *iter, 16, sm3.New)
-	keyHex = hex.EncodeToString(prvRaw)
-	} else {
-	keyHex = *key
-	}
-	key, err := hex.DecodeString(keyHex)
-	if err != nil {
-                log.Fatal(err)
-	}
-	h := hmac.New(sm3.New, key)
-	if _, err = io.Copy(h, os.Stdin); err != nil {
-                log.Fatal(err)
-	}
-	fmt.Println(hex.EncodeToString(h.Sum(nil)))
-        os.Exit(0)
-        }
-
-
-        if *target != "" && *recursive == false {
-	files, err := filepath.Glob(*target)
-	if err != nil {
-	    log.Fatal(err)
-	}
-
-	for _, match := range files {
-	h := sm3.New()
-        f, err := os.Open(match)
-        if err != nil {
-            log.Fatal(err)
-        }
-        if _, err := io.Copy(h, f); err != nil {
-            log.Fatal(err)
-        }
-    	fmt.Println(hex.EncodeToString(h.Sum(nil)), "*" + f.Name())
-	}
-	}
-
 
 	if *target != "" && *recursive == true {
 		err := filepath.Walk(filepath.Dir(*target),
-		func(path string, info os.FileInfo, err error) error {
-			if err != nil {
-				return err
-			}
-			file, err := os.Stat(path)
-			if file.IsDir() {
-			} else {
-				filename := filepath.Base(path)
-				pattern := filepath.Base(*target)
-				matched, err := filepath.Match(pattern, filename)
+			func(path string, info os.FileInfo, err error) error {
 				if err != nil {
-					fmt.Println(err)
+					return err
 				}
-				if matched {
-				h := sm3.New()
-			        f, err := os.Open(path)
-			        if err != nil {
-			            log.Fatal(err)
-			        }
-				if _, err := io.Copy(h, f); err != nil {
-					log.Fatal(err)
+				file, err := os.Stat(path)
+				if file.IsDir() {
+				} else {
+					filename := filepath.Base(path)
+					pattern := filepath.Base(*target)
+					matched, err := filepath.Match(pattern, filename)
+					if err != nil {
+						fmt.Println(err)
+					}
+					if matched {
+						h := sm3.New()
+						f, err := os.Open(path)
+						if err != nil {
+							log.Fatal(err)
+						}
+						if _, err := io.Copy(h, f); err != nil {
+							log.Fatal(err)
+						}
+						fmt.Println(hex.EncodeToString(h.Sum(nil)), "*"+f.Name())
+					}
 				}
-				fmt.Println(hex.EncodeToString(h.Sum(nil)), "*" + f.Name())
-				}
-			}
-			return nil
-		})
+				return nil
+			})
 		if err != nil {
 			log.Println(err)
 		}
 	}
 
+	if *check != "" {
+		file, err := os.Open(*check)
 
-        if *check != "" {
-	file, err := os.Open(*check)
- 
-	if err != nil {
-		log.Fatalf("failed opening file: %s", err)
-	}
- 
-	scanner := bufio.NewScanner(file)
-	scanner.Split(bufio.ScanLines)
-	var txtlines []string
- 
-	for scanner.Scan() {
-		txtlines = append(txtlines, scanner.Text())
-	}
- 
-	file.Close()
- 
-
-	for _, eachline := range txtlines {
-	lines := strings.Split(string(eachline), " *")
-
-	if strings.Contains(string(eachline), " *") {
-
-	h := sm3.New()
-
-	_, err := os.Stat(lines[1])
-	if err == nil {
-		f, err := os.Open(lines[1])
 		if err != nil {
-		     log.Fatal(err)
+			log.Fatalf("failed opening file: %s", err)
 		}
-		io.Copy(h, f)
-		
-		if *verbose {
-			if hex.EncodeToString(h.Sum(nil)) == lines[0] {
-				fmt.Println(lines[1] + "\t", "OK")
-			} else {
-				fmt.Println(lines[1] + "\t", "FAILED")
-			}
-		} else {
-			if hex.EncodeToString(h.Sum(nil)) == lines[0] {
-			} else {
-				os.Exit(1)
-			}
-		}
-	} else {
-		if *verbose {
-			fmt.Println(lines[1] + "\t", "Not found!")
-		} else {
-			os.Exit(1)	
-		}	
-	}
-	}
-	}
 
+		scanner := bufio.NewScanner(file)
+		scanner.Split(bufio.ScanLines)
+		var txtlines []string
+
+		for scanner.Scan() {
+			txtlines = append(txtlines, scanner.Text())
+		}
+
+		file.Close()
+
+		for _, eachline := range txtlines {
+			lines := strings.Split(string(eachline), " *")
+
+			if strings.Contains(string(eachline), " *") {
+
+				h := sm3.New()
+
+				_, err := os.Stat(lines[1])
+				if err == nil {
+					f, err := os.Open(lines[1])
+					if err != nil {
+						log.Fatal(err)
+					}
+					io.Copy(h, f)
+
+					if *verbose {
+						if hex.EncodeToString(h.Sum(nil)) == lines[0] {
+							fmt.Println(lines[1]+"\t", "OK")
+						} else {
+							fmt.Println(lines[1]+"\t", "FAILED")
+						}
+					} else {
+						if hex.EncodeToString(h.Sum(nil)) == lines[0] {
+						} else {
+							os.Exit(1)
+						}
+					}
+				} else {
+					if *verbose {
+						fmt.Println(lines[1]+"\t", "Not found!")
+					} else {
+						os.Exit(1)
+					}
+				}
+			}
+		}
 	}
 }
